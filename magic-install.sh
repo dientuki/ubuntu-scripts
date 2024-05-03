70305b36a8af0694b0bdd73fbb1d4e5593db5c35
Juan Farias <dientuki@gmail.com>
2024-05-03 18:00:21 -0300

    Update magic-install.sh

--- magic-install.sh
+++ magic-install.sh
@@ -6,15 +6,22 @@ RED='\033[31m'
 GREEN='\033[32m'
 YELLOW='\033[33m'
 ENDCOLOR='\033[0m'
-term_width=$(tput cols)
+
+# Store the current user
+CURRENT_USER="$USER"
 
 if [ $USER != root ]; then
-  echo -e $RED"Error: must be root"
+  echo -e $RED"Error: must be root"$ENDCOLOR
   echo -e $YELLOW"Exiting..."$ENDCOLOR
   exit 0
 fi
 
-cd $HOME
+# Check if argument is provided
+if [ $# -eq 0 ]; then
+  echo -e $RED"Error: Argument missing. Please provide a folder name to install Magic Mirror."$ENDCOLOR
+  exit 1
+fi
+MMPATH="$1"
 
 echo 'Remove screensaver'
 apt-get purge xscreensaver*
@@ -22,13 +29,27 @@ apt-get purge xscreensaver*
 echo 'Tools'
 apt-get install ca-certificates apt-transport-https gnupg curl
 
-echo 'Chrome'
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-dpkg -i google-chrome-stable_current_amd64.deb
-rm -rf google-chrome-stable_current_amd64.deb
+# Validation for Chrome installation
+read -p "Do you want to install Google Chrome? [Y/n]: " INSTALL_CHROME
+INSTALL_CHROME=${INSTALL_CHROME:-Y}  # default is Yes
+
+if [[ $INSTALL_CHROME =~ ^[Yy]$ ]]; then
+  echo 'Try to install Chrome'
+  if wget -q --spider https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb; then
+    wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+    dpkg -i google-chrome-stable_current_amd64.deb
+    rm -rf google-chrome-stable_current_amd64.deb
+    echo -e $GREEN"Error: must be root"$ENDCOLOR
+  else
+    echo -e "${YELLOW}Google Chrome download failed. Skipping installation.${ENDCOLOR}"
+  fi
+else
+  echo "Skipping Chrome installation."
+fi
 
 echo 'Spotify'
 curl -sS https://download.spotify.com/debian/pubkey_6224F9941A8AA6D1.gpg | gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 echo 'Node 20'
 curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
@@ -43,10 +64,15 @@ apt-get update
 apt-get autoremove
 apt-get install nodejs webmin spotify-client samba -y
 
-echo 'Magic mirror'
-git clone https://github.com/MagicMirrorOrg/MagicMirror camouflage
-cd camouflage
-npm run install-mm
+echo 'Magic mirror, switching back to the original user'
 
-chown root $HOME/camouflage/node_modules/electron/dist/chrome-sandbox
-chmod 4755 $HOME/camouflage/node_modules/electron/dist/chrome-sandbox
+# Switch back to the original user
+su -c '
+cd $HOME
+git clone https://github.com/MagicMirrorOrg/MagicMirror $MMPATH
+cd $MMPATH
+npm run install-mm
+chown root $HOME/$MMPATH/node_modules/electron/dist/chrome-sandbox
+chmod 4755 $HOME/$MMPATH/node_modules/electron/dist/chrome-sandbox
+cp config/config.js.sample config.js
+' "$USER"
